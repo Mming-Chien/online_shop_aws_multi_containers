cd87093dd08eddd67aa391f154371969b1111d0b
Mming-Chien <113403394+Mming-Chien@users.noreply.github.com>
2022-12-23 23:13:09 +0700

    including order models in the administration site

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+class OrderItemLine(admin.TabularInline):
+	''' Inline to display in OrderAdmin edit page '''
+	model = OrderItem 
+	raw_id_fields = ['product']
+
+@admin.register(Order)
+class OderAdmin(admin.ModelAdmin):
+	list_display = ['id', 'first_name', 'last_name', 'email', 'addess', 
+			'postal_code', 'city', 'paid', 'updated', 'created']
+	list_filter = ['paid', 'created', 'updated']
+	inlines = [OrderItemLine]
