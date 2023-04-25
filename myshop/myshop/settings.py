f2defed172e0e5b10d6b3daf5cc133f556230ba7
Mming-Chien <113403394+Mming-Chien@users.noreply.github.com>
2023-04-25 17:43:38 +0700

    fix heroku deployment 7

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -90,8 +90,10 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myshop',
+        'USER': 'myshop',
+        'PASSWORD': '123456789',
     }
 }
 
