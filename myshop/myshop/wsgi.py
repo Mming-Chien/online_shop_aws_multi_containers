08a57789b4076d529eedecfc683ecb8ef8b8a770
Mming-Chien <113403394+Mming-Chien@users.noreply.github.com>
2023-04-25 23:32:50 +0700

    30

--- myshop/myshop/wsgi.py
+++ myshop/myshop/wsgi.py
@@ -9,13 +9,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
-import os
-import sys
-
-root_path = os.path.abspath(os.path.split(__file__)[0])
-sys.path.insert(0, os.path.join(root_path, 'myshop'))
-sys.path.insert(0, root_path)
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
